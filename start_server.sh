9ba6bdde4f0779b1b82bf6d2a007ca678d7e42b9
Cao Jiafeng <funfriendcjf@gmail.com>
2016-12-19 17:44:41 +0800

    add docker files

--- start_server.sh
+++ start_server.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 exec java \
+     $JMX_OPTS \
      -Dlog4j.configuration=file:/ambry/config/log4j.properties \
      -jar ambry.jar \
      --serverPropsFilePath /ambry/config/server.properties \
      --hardwareLayoutFilePath /ambry/config/HardwareLayout.json \
      --partitionLayoutFilePath /ambry/config/PartitionLayout.json
+
